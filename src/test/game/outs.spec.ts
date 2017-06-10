5b3180c4dc5ae7872e3f4ba3f5c85bfb58f7d7ae
Andrzej Gustyn <panguzol@gmail.com>
2017-06-10 22:16:18 +0100

    inside straight

--- src/test/game/outs.spec.ts
+++ src/test/game/outs.spec.ts
@@ -2,6 +2,7 @@ import * as R from "ramda"
 import { getOuts } from "../../game/outs"
 import { getNewDeck } from "../../poker/dealer"
 import { sortCards, cardsFromShort } from "../../poker/cards"
+import { Card } from "../../poker/types"
 import * as A from "assert"
 
 
@@ -10,9 +11,21 @@ describe("Outs counter", () => {
     it("should return remaing two cards for pocket pair", () => {
         let community = cardsFromShort([ "Th", "4c", "Qd" ])
         let player = cardsFromShort(["8c", "8h"])
-        let remainingCards = R.difference(getNewDeck(), community.concat(player))
         let expectedOuts = cardsFromShort([ "8d", "8s"])
-        let outs = getOuts(community, player, remainingCards)
-        A.equal(sortCards(outs), sortCards(expectedOuts))
+        runTest(community, player, expectedOuts)
+    })
+
+    it("should return 4 cards for inside straight", () => {
+        let community = cardsFromShort(["4h", "7c", "As", "Jh"])
+        let player = cardsFromShort(["5s", "8d"])
+        let expectedOuts = cardsFromShort(["6d", "6h", "6c", "6s"])
+        runTest(community, player, expectedOuts)
     })
 })
+
+
+const runTest = (community: Card[], player: Card[], expectedOuts: Card[]) => {
+    let remainingCards = R.difference(getNewDeck(), community.concat(player))
+    let outs = getOuts(community, player, remainingCards)
+    A.equal(sortCards(outs), sortCards(expectedOuts))
+}
