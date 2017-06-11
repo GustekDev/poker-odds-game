4b027fa78f1ffbad68a0b39736f21742627f092a
Andrzej Gustyn <panguzol@gmail.com>
2017-06-11 15:14:26 +0100

    OutsPracticeComonent wip

---
+++ src/game/components/OutsPracticeComponent.tsx
@@ -0,0 +1,21 @@
+import * as React from "react"
+
+import { Card, Cards } from "../../poker/types"
+
+interface Props {
+    cards: Cards;
+    outs: Card[];
+}
+
+interface State {
+    guess?: number;
+}
+
+export default class OutsPracticeComponent extends React.Component<Props, State> {
+
+    render () {
+        return (<div>
+            <label>How many outs do you have?<input type="number" /></label>
+        </div>)
+    }
+}
