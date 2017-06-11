87304fcb8213da6ba7e06e5c223609e39ea6eb1a
Andrzej Gustyn <panguzol@gmail.com>
2017-06-11 16:18:46 +0100

    outs practice wip

--- src/game/components/OutsPracticeComponent.tsx
+++ src/game/components/OutsPracticeComponent.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as R from 'ramda';
 
 import { Card, Cards } from '../../poker/types';
 
@@ -9,14 +10,49 @@ interface Props {
 
 interface State {
     guess?: number;
+    showAnswer: boolean;
 }
 
 export default class OutsPracticeComponent extends React.Component<Props, State> {
 
+    constructor(props: Props) {
+        super(props);
+        this.state = { showAnswer: false }
+    }
+
+    guess = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        this.check();
+    }
+
+    setGuess = (guess: number) => {
+        this.setState((prev: State) => R.merge(prev, {guess}))
+    }
+
+    check = () => {
+        this.setState((prev) => R.merge(prev, {showAnswer: true}))
+    }
+
+    renderAnswer = (state: State) => {
+        let outs = this.props.outs.length;
+        if (state.showAnswer) {
+            return (
+                <div>
+                    <span>{state.guess === outs ? "Correct" : "Wrong. There are " + outs}</span>
+                </div>
+            )
+        }
+        return null;
+    }
+
     render() {
         return (
             <div>
+                <form onSubmit={(event) => this.guess(event)}>
                 <label>How many outs do you have?<input type="number" /></label>
+                <input type="submit" value="Guess" />
+                </form>
+                {this.renderAnswer(this.state)}
             </div>
         );
     }
