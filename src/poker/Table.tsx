bba074ffe03862d4691b500eda8c7bf2ec25cfbd
Andrzej Gustyn <panguzol@gmail.com>
2017-07-02 13:50:34 +0100

    green

--- src/poker/Table.tsx
+++ src/poker/Table.tsx
@@ -10,10 +10,18 @@ interface Props {
 
 export default class Table extends React.Component<Props, null> {
 
+    renderUnknown(dealtCount: number): JSX.Element[] {
+        var cards = [];
+        for(let i = 0; i < 5 - dealtCount; i++) {
+            cards.push(<div className="card back">*</div>)
+        }
+        return cards;
+    }
+
     render() {
         return (
             <div>
-                Community: {displayCards(this.props.community)}
+                Community: {displayCards(this.props.community)}{this.renderUnknown(this.props.community.length)}
                 Your cards: {displayCards(this.props.player)}
             </div>
         );
